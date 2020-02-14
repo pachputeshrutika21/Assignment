c77ee12c69626b5dbba1d5617bfa3d8e56a8af17
pachputeshrutika21 <52059643+pachputeshrutika21@users.noreply.github.com>
2020-02-14 17:29:22 +0530

    commit

---
+++ Beginner-javascript/custom-function/customfun.js
@@ -0,0 +1,20 @@
+/* eslint-disable*/
+// console.log("it works!!!");
+// let bill=100;
+// const works=1.13;
+function custombill(bill,works)
+{
+    console.log("hii i am try to function!!!");//fuction body
+    const total=bill*works;
+    return total;
+}
+const mytotal=custombill(11,3);
+bill=200;
+const mytotal1=custombill(22,4);
+console.log(`your total is $${mytotal}, $${mytotal1}`);
+
+function myname(firstname)
+{
+   console.log(`Hello ${firstname}`);
+}
+myname('qliktag');
--- Beginner-javascript/function.js
+++ Beginner-javascript/function.js
@@ -1,2 +1,23 @@
 /* eslint-disable*/
 //built in functions
+//maximum value without passing parameter
+Math.max()
+-Infinity
+//to find the maximum of two no
+Math.max(10,12)
+
+//using floor
+Math.floor(2.444)
+2
+//check the now date
+Date.now()
+1581672748027
+//string to float
+parseFloat('20.3456765')
+20.3456765
+//int to float
+parseInt('20.23235')
+
+//navigator vibrate 
+navigator.vibrate(200)
+true
