0126ebccb186f10ced48ad6db2b2cafe243bdccc
pachputeshrutika21 <52059643+pachputeshrutika21@users.noreply.github.com>
2020-02-19 17:28:15 +0530

    commit

--- Beginner-javascript/calculator.js
+++ Beginner-javascript/calculator.js
@@ -2,9 +2,9 @@
 const butt=document.querySelector('.butt');
 function calculate()
 {
-    var first=document.getElementById('first').value;
+    var first=parseInt(document.getElementById('first').value);
     console.log(first);
-    var second=document.getElementById('second').value;
+    var second=parseInt(document.getElementById('second').value);
     console.log(second);
     var operator=document.getElementById('operator').value;
     if(operator==='+')
