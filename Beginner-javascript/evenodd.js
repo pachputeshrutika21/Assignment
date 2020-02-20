f345481645df1815d1d5b7e5094cf0b1f810c62c
pachputeshrutika21 <52059643+pachputeshrutika21@users.noreply.github.com>
2020-02-20 15:49:03 +0530

    commit

---
+++ Beginner-javascript/evenodd.js
@@ -0,0 +1,17 @@
+/* eslint-disable*/
+const butts=document.querySelector('.button');
+function clickedNumber()
+{
+    var no1=parseInt(document.getElementById('n1').value);
+    console.log(`value is ${no1}`);
+
+    if(no1%2==0)
+    {
+        document.getElementById('res').innerHTML=`Number ${no1} is even`;
+    }
+    else{
+        document.getElementById('res').innerHTML=`Number ${no1} is odd`;
+    }
+
+}
+butts.addEventListener('click',clickedNumber);
