4d83180340d00b716432c218412ace591f664265
pachputeshrutika21 <52059643+pachputeshrutika21@users.noreply.github.com>
2020-02-20 11:41:37 +0530

    commit

---
+++ Beginner-javascript/Events/js-5bcywf - StackBlitz_files/index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700|Lato:400,700,900" rel="stylesheet">
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  
+  <script src="https://c.staticblitz.com/assets/common-05af4db16452907fb840b.js" crossorigin="anonymous"></script>
+  <script src="https://c.staticblitz.com/assets/ext-e3308bdcc0bd1e06d404b.js" crossorigin="anonymous"></script>
+  <script src="https://c.staticblitz.com/d/webcontainer.54bab4834f1d9316536.js" crossorigin="anonymous"></script>
+  <script src="https://c.staticblitz.com/assets/preview-3556fde54a8ea24f101bb.js" crossorigin="anonymous"></script>
+  <script>(function(){_preboot("https://l.staticblitz.com/b/v1/js-5bcywf/98697c87429",{p:"stackblitz",a:"AIzaSyBZSvuCzbUhuRrSps-HjM5bFClLPaFF9Vg",o:true})})()</script>
+</head>
+<body></body>
+</html>
